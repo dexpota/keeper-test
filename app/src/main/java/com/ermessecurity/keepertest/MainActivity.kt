6259bc8fc8d9123853c5dba4f80136761704d912
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2020-10-28 15:56:45 +0100

    First commit

---
+++ app/src/main/java/com/ermessecurity/keepertest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ermessecurity.keepertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
